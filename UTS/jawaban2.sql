f863143755e6292f6d2b5997a617d7b4ca440ddd
Aryokresnonugroho <151042544+Aryokresnonugroho@users.noreply.github.com>
2023-11-16 22:02:57 +0700

    jawaban2.sql

---
+++ UTS/jawaban2.sql
@@ -0,0 +1,11 @@
+ INSERT INTO mahasiswa (id, nama, matkul_id, nim) VALUES
+ ('CR.A.401', 'Aryo kresno nugroho', '123', '20220801209'),
+ ('CR.A.301', 'Bayu Sukmo Adji', '321', '20220801089');
+ 
+ INSERT INTO dosen (id, nama, kode_dosen, matkul_id ) VALUES
+ (23, 'Iwan fals', '1021','123' ),
+ (41, 'Cecep markecep', '2142', '321');
+ 
+ insert INTO mata_kuliah (id, nama_mata_kuliah, sks, matkul_id) VALUES
+ (5, 'Basis data', '3', '123'), 
+ (2, 'Algoritma', '3', '321');
